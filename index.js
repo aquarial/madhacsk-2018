4c9875093980d43b18528aa82f9c1eab2cbe9fbe
Karl <37496339+aquarial@users.noreply.github.com>
2018-11-10 20:50:35 -0600

    Changes

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
+app.use(express.static(__dirname));
+
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var readConfig = require('read-config');
@@ -6,31 +9,32 @@ var config = readConfig('./config.json');
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
-
 client.on('message', msg => {
-    if (msg.content === 'dec') {
-        io.emit('decrease');
-    }
-    if (msg.content === 'inc') {
-        io.emit('increase');
+    if (msg.guild !== null && msg.guild.available && msg.guild.id === "453207241294610442") {
+        var a = { authorid: msg.author.id, chanid: msg.channel.id, channame: msg.channel.name };
+        io.emit("msg", a);
     }
 });
-//io.on('connection', function(socket){
-//  console.log('a user connected');
-//  io.emit('increase');
-//  socket.on('disconnect', function(){
-//    console.log('user disconnected');
-//    io.emit('decrease');
-//  });
-//});
+client.on("ready", () => {
+  app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+  });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+  io.on('connection', function(socket){
+    //console.log('a user connected');
+    //io.emit('increase');
+      socket.emit("channels", [...client.guilds.get(config.guild).channels].map(a => a[1]));
+    socket.on('disconnect', function(){
+      //console.log('user disconnected');
+      //io.emit('decrease');
+    });
+  });
+
+  http.listen(3000, function(){
+    console.log('listening on *:3000');
+  });
+
+
 });
-
 
 client.login(config.token);
